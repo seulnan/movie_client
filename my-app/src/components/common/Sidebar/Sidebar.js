ddec156d32e634d4fa3646ffc2c9eb8bc07c73e3
Yoon seungjoo <seungjoo0o6272@gmail.com>
2025-01-20 21:34:38 +0900

    [Fix] 사이드바 호버, 클릭시 이미지 변경
    [Fix] 사이드바 호버, 클릭시 이미지 변경
    
    사이드바 아이콘 클릭시 이미지가 바뀌지 않아, 수정함

--- my-app/src/components/common/Sidebar/Sidebar.js
+++ my-app/src/components/common/Sidebar/Sidebar.js
@@ -15,6 +15,7 @@ import BookmarkHoverIcon from "../../../assets/Bookmark2.svg";
 
 const Sidebar = () => {
   const [activeIcon, setActiveIcon] = useState(null); // 클릭한 아이콘 상태 관리
+  const [hoverIcon, setHoverIcon] = useState(null); // 호버 상태 관리
 
   const menuItems = [
     { name: "home", default: HomeIcon, hover: HomeHoverIcon, path: "/home" },
@@ -33,12 +34,18 @@ const Sidebar = () => {
           key={item.name}
           to={item.path}
           className="icon"
-          onMouseEnter={() => setActiveIcon(item.name)}
-          onMouseLeave={() => setActiveIcon(null)}
-          onClick={() => setActiveIcon(item.name)}
+          onMouseEnter={() => setHoverIcon(item.name)} // 마우스가 아이콘 위에 올라가면 호버 상태로
+          onMouseLeave={() => setHoverIcon(null)} // 마우스가 아이콘을 떠나면 호버 상태 해제
+          onClick={() => setActiveIcon(item.name)} // 클릭하면 active 상태로 설정
         >
           <img
-            src={activeIcon === item.name ? item.hover : item.default}
+            src={
+              activeIcon === item.name // 클릭된 아이콘이면
+                ? item.hover
+                : hoverIcon === item.name // 호버된 아이콘이면
+                ? item.hover
+                : item.default // 기본 상태
+            }
             alt={`${item.name} Icon`}
           />
         </NavLink>
