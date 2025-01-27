a93b61641c947a567b8b3c77bbdcb86562ab7aef
Yoon seungjoo <seungjoo0o6272@gmail.com>
2025-01-27 17:33:49 +0900

    [Fix] 서치바 상태별 디자인 수정
    Fix : 서치바 상태별 디자인 수정
    
    서치바 입력 전, 입력 중, 검색 후 디자인 피그마에 맞게 수정하였고
    돋보기 아이콘도 겹치지 않게 수정했습니다.

--- my-app/src/components/common/SearchBar/SearchBar.js
+++ my-app/src/components/common/SearchBar/SearchBar.js
@@ -3,12 +3,19 @@ import "./SearchBar.css";
 import searchIcon from "../../../assets/search.svg";
 
 const SearchBar = React.memo(({ onSearch }) => {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState(""); // 검색어 상태
+  const [isSearchComplete, setIsSearchComplete] = useState(false); // 검색 완료 상태
 
   // 입력값 변경 핸들러
   const handleInputChange = (e) => {
     const value = e.target.value;
     setQuery(value);
+
+    // 입력이 변경되면 검색 완료 상태 초기화
+    if (isSearchComplete) {
+      setIsSearchComplete(false);
+    }
+
     if (value.trim() === "") {
       onSearch(""); // 검색어가 없으면 초기화
     }
@@ -18,6 +25,7 @@ const SearchBar = React.memo(({ onSearch }) => {
   const executeSearch = useCallback(() => {
     if (query.trim() !== "") {
       onSearch(query);
+      setIsSearchComplete(true); // 검색 완료 상태 설정
     }
   }, [query, onSearch]);
 
@@ -38,7 +46,7 @@ const SearchBar = React.memo(({ onSearch }) => {
       />
       <input
         type="text"
-        className="search-input"
+        className={`search-input ${isSearchComplete ? "search-complete" : ""}`} // 검색 완료 상태에 따라 클래스 변경
         placeholder="Search for movies or TV series"
         value={query}
         onChange={handleInputChange}
